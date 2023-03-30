b601784863640f9c10d24aafeb177a097d8ea671
Fashina Segun <fashina.segun25@gmail.com>
2023-03-30 01:30:59 +0100

    last refactors

--- youtube-search/src/App.js
+++ youtube-search/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import YTSearch from 'youtube-api-search';
+// Components
+import SearchBar from './components/SearchBar/SearchBar';
+import VideoList from './components/VideoList/VideoList';
+import VideoDetail from './components/VideoDetail/VideoDetail';
+import Nav from './components/Nav/Nav';
+// styles
+import { Container } from './appStyles.js';
+// Personal Key, it would be hidden for you. Get One!
+import API_KEY from './key';
 
 function App() {
+  const [videos, setVideos] = useState(null),
+    [selectedVideo, setSelectVideo] = useState();
+
+  const handleSearch = term =>
+    YTSearch({ key: API_KEY, term }, videos => {
+      setVideos(videos);
+      setSelectVideo(videos[0]);
+    });
+
+  useEffect(() => {
+    handleSearch('Guns and roses');
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Nav>
+        <SearchBar handleSearch={handleSearch} />
+      </Nav>
+      <VideoDetail videos={selectedVideo}>
+        <VideoList
+          videos={videos}
+          handleSelectVideo={selectedVideo => setSelectVideo(selectedVideo)}
+        />
+      </VideoDetail>
+    </Container>
   );
 }
 
